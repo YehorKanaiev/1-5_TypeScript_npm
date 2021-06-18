91420aad5a2fe61ce683af24a9daa08c4b2d2304
YehorKanaiev <egor15kanaev@gmail.com>
2021-06-18 21:27:27 +0300

    Adds timer

---
+++ src/main.ts
@@ -0,0 +1,63 @@
+import moment from 'moment';
+
+const RELOAD_DELAY: number = 200 //ms
+const MINUTE: number = 60 //s
+const ON_WORK_LABEL: string = 'Осталось';
+const WAIT_LABEL: string = 'Укажите время в минутах';
+
+const startButton = document.getElementById('startButton');
+const plusButton = document.getElementById('plusButton');
+const minusButton = document.getElementById('minusButton');
+const display = document.getElementById('display');
+const timerDescription = document.getElementById('timerDescription');
+let isBlocked: boolean = false;
+
+
+plusButton.addEventListener('click', (): void => {
+  if (isBlocked) {
+    return;
+  }
+  display.innerText = (parseInt(display.innerText) + 1) + "";
+})
+
+minusButton.addEventListener('click', (): void => {
+  if (isBlocked) {
+    return;
+  }
+  let currentValue: number = parseInt(display.innerText);
+  if (currentValue > 0) {
+    display.innerText = currentValue - 1 + ""
+  }
+})
+
+startButton.addEventListener('click', ():void => {
+  if (isBlocked) {
+    return;
+  }
+  isBlocked = true;
+  let duration:number = parseInt(display.innerText);
+  let startTime: number = moment().unix();
+  let endTime:number = startTime + duration * MINUTE;
+
+  let counter: number = 0;
+  display.innerText = moment.unix(duration * MINUTE).format('mm:ss');
+  timerDescription.innerText = ON_WORK_LABEL;
+
+  countTime();
+
+  function countTime(): void {
+    let currentTime: number = moment().unix();
+    if (currentTime < endTime) {
+      if ((currentTime - startTime) > counter) {
+        counter += 1;
+        display.innerText = moment.unix(duration * MINUTE - counter).format('mm:ss');
+      }
+      setTimeout(countTime, RELOAD_DELAY)
+    } else {
+      timerDescription.innerText = WAIT_LABEL;
+      display.innerText = duration + "";
+      isBlocked = false;
+    }
+  }
+})
+
